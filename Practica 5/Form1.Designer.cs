75ad419600527fdd076240f02d304d322543251c
KARLOSR4 <javier.venturadiaz@unidemat.edu.mx>
2025-02-20 16:49:28 -0600

    Practica 5
    Calificaciones

---
+++ Practica 5/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Practica_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbla = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btne = new System.Windows.Forms.Button();
+            this.txtalum = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txts = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt3);
+            this.groupBox1.Controls.Add(this.txts);
+            this.groupBox1.Controls.Add(this.txt1);
+            this.groupBox1.Controls.Add(this.txtalum);
+            this.groupBox1.Controls.Add(this.btne);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lbla);
+            this.groupBox1.Location = new System.Drawing.Point(27, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(747, 200);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sistema de Calificaciones";
+            // 
+            // lbla
+            // 
+            this.lbla.AutoSize = true;
+            this.lbla.Location = new System.Drawing.Point(22, 49);
+            this.lbla.Name = "lbla";
+            this.lbla.Size = new System.Drawing.Size(52, 16);
+            this.lbla.TabIndex = 0;
+            this.lbla.Text = "Alumno";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(388, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1 Parcial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(388, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2 Parcial";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(388, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3 Parcial";
+            // 
+            // btne
+            // 
+            this.btne.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btne.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btne.Location = new System.Drawing.Point(125, 141);
+            this.btne.Name = "btne";
+            this.btne.Size = new System.Drawing.Size(99, 37);
+            this.btne.TabIndex = 4;
+            this.btne.Text = "Evaluar";
+            this.btne.UseVisualStyleBackColor = false;
+            this.btne.Click += new System.EventHandler(this.btne_Click);
+            // 
+            // txtalum
+            // 
+            this.txtalum.Location = new System.Drawing.Point(100, 49);
+            this.txtalum.Name = "txtalum";
+            this.txtalum.Size = new System.Drawing.Size(154, 22);
+            this.txtalum.TabIndex = 5;
+            this.txtalum.TextChanged += new System.EventHandler(this.txtalum_TextChanged);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(504, 15);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(77, 22);
+            this.txt1.TabIndex = 6;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // txts
+            // 
+            this.txts.Location = new System.Drawing.Point(506, 74);
+            this.txts.Name = "txts";
+            this.txts.Size = new System.Drawing.Size(75, 22);
+            this.txts.TabIndex = 7;
+            this.txts.TextChanged += new System.EventHandler(this.txts_TextChanged);
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(506, 141);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(75, 22);
+            this.txt3.TabIndex = 8;
+            this.txt3.TextChanged += new System.EventHandler(this.txt3_TextChanged);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(27, 247);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(747, 175);
+            this.dgv.TabIndex = 1;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtalum;
+        private System.Windows.Forms.Button btne;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbla;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txts;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ Practica 5/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_5
+{
+    public partial class Form1 : Form
+    {
+        int promedio;//Declaro la variable promedio en la cual ira la ecuacion para sacar el promedio de los 3 parciales
+
+        string Estatus;//La variable estatus definira que dependiendo de la calificacion evaluara si les corresponde Exento,Ordinario,Extraordinario,o Especial
+        public Form1()
+        {
+            InitializeComponent();
+            //Esta parte de aqui es para crear las columnas donde iran el nombre del alumno,calificacion de los 3 parciales,promedio y estatus
+            //Se ponen dos veces el nombre ya que uno es para lo que se mostrara en pantalla y el otro es con lo que se identifica en el codigo
+            dgv.Columns.Add("Alumno", "Alumno");
+            dgv.Columns.Add("Parcial 1", "Parcial 1");
+            dgv.Columns.Add("Parcial 2", "Parcial 2");
+            dgv.Columns.Add("Parcail 3", "Parcial 3");
+            dgv.Columns.Add("Promedio", "Promedio");
+            dgv.Columns.Add("Estatus", "Estatus");
+        }
+
+        private void btne_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtalum.Text) ||
+                string.IsNullOrWhiteSpace(txt1.Text) ||
+                string.IsNullOrWhiteSpace(txts.Text) ||
+                string.IsNullOrWhiteSpace(txt3.Text))
+            {
+                //este messagebox es el mostrado en dado caso que se cumpla la condicion del if anterior
+                MessageBox.Show("Por favor, complete todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //en esta parte se hace la hace la ecuacion que le corresponde a promedio sumando los 3 calificaciones y dividiendola entre 3
+            promedio = (Convert.ToInt32(txt1.Text) + Convert.ToInt32(txts.Text) + Convert.ToInt32(txt3.Text)) / 3;
+            //esta parte es donde hago la creacion de todos los if y pongo condiciones a la variable promedio,que depende del valor ya sea mayor o igual es el estatus que les corresponde
+            if (promedio >= 95)
+            {
+                Estatus = "Exento";
+            }
+            if (promedio >= 70 && promedio <= 94)
+                Estatus = "Ordinaria";
+            if (promedio >= 50 && promedio <= 69)
+                Estatus = "Extraordinario";
+            if (promedio <= 49)
+                Estatus = "Especial";
+            //con la variable vIndice voy añadiendo nuevas filas
+            //En esta parte de aqui voy creando celdas y les doy un valor a cada una en el caso de la primera seria el valor guardado en txtalumno.Text
+            int vIndice = dgv.Rows.Add();
+            dgv.Rows[vIndice].Cells[0].Value = txtalum.Text;
+            dgv.Rows[vIndice].Cells[1].Value = Convert.ToInt32(txt1.Text);
+            dgv.Rows[vIndice].Cells[2].Value = Convert.ToInt32(txts.Text);
+            dgv.Rows[vIndice].Cells[3].Value = Convert.ToInt32(txt3.Text);
+            dgv.Rows[vIndice].Cells[4].Value = promedio;
+            dgv.Rows[vIndice].Cells[5].Value = Estatus;
+
+        //Esta parte de aqui es para que cuando presione el boton llamado calcular automaticamente queden las cassilas en blanco
+            txtalum.Text = "";
+            txt1.Text = "";
+            txts.Text = "";
+            txt3.Text = "";
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txts_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtalum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
