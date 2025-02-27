736f91f25f42328ec49a252064865c34c3934f49
KARLOSR4 <javier.venturadiaz@unidemat.edu.mx>
2025-02-26 18:05:35 -0600

    Examen
    Examen Signo Zodiacal

---
+++ Examen/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.r1 = new System.Windows.Forms.RadioButton();
+            this.r2 = new System.Windows.Forms.RadioButton();
+            this.r3 = new System.Windows.Forms.RadioButton();
+            this.r4 = new System.Windows.Forms.RadioButton();
+            this.r5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.r7 = new System.Windows.Forms.RadioButton();
+            this.r8 = new System.Windows.Forms.RadioButton();
+            this.r9 = new System.Windows.Forms.RadioButton();
+            this.r10 = new System.Windows.Forms.RadioButton();
+            this.r11 = new System.Windows.Forms.RadioButton();
+            this.r12 = new System.Windows.Forms.RadioButton();
+            this.Combo = new System.Windows.Forms.ComboBox();
+            this.lbli = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.r12);
+            this.groupBox1.Controls.Add(this.r11);
+            this.groupBox1.Controls.Add(this.r10);
+            this.groupBox1.Controls.Add(this.r9);
+            this.groupBox1.Controls.Add(this.r8);
+            this.groupBox1.Controls.Add(this.r7);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.r5);
+            this.groupBox1.Controls.Add(this.r4);
+            this.groupBox1.Controls.Add(this.r3);
+            this.groupBox1.Controls.Add(this.r2);
+            this.groupBox1.Controls.Add(this.r1);
+            this.groupBox1.Location = new System.Drawing.Point(29, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 370);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mes de nacimiento";
+            // 
+            // r1
+            // 
+            this.r1.AutoSize = true;
+            this.r1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r1.Location = new System.Drawing.Point(6, 48);
+            this.r1.Name = "r1";
+            this.r1.Size = new System.Drawing.Size(74, 24);
+            this.r1.TabIndex = 0;
+            this.r1.TabStop = true;
+            this.r1.Text = "Enero";
+            this.r1.UseVisualStyleBackColor = true;
+            this.r1.CheckedChanged += new System.EventHandler(this.r1_CheckedChanged);
+            // 
+            // r2
+            // 
+            this.r2.AutoSize = true;
+            this.r2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r2.Location = new System.Drawing.Point(6, 74);
+            this.r2.Name = "r2";
+            this.r2.Size = new System.Drawing.Size(88, 24);
+            this.r2.TabIndex = 1;
+            this.r2.TabStop = true;
+            this.r2.Text = "Febrero";
+            this.r2.UseVisualStyleBackColor = true;
+            this.r2.CheckedChanged += new System.EventHandler(this.r2_CheckedChanged);
+            // 
+            // r3
+            // 
+            this.r3.AutoSize = true;
+            this.r3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r3.Location = new System.Drawing.Point(6, 100);
+            this.r3.Name = "r3";
+            this.r3.Size = new System.Drawing.Size(77, 24);
+            this.r3.TabIndex = 2;
+            this.r3.TabStop = true;
+            this.r3.Text = "Marzo";
+            this.r3.UseVisualStyleBackColor = true;
+            this.r3.CheckedChanged += new System.EventHandler(this.r3_CheckedChanged);
+            // 
+            // r4
+            // 
+            this.r4.AutoSize = true;
+            this.r4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r4.Location = new System.Drawing.Point(6, 126);
+            this.r4.Name = "r4";
+            this.r4.Size = new System.Drawing.Size(64, 24);
+            this.r4.TabIndex = 3;
+            this.r4.TabStop = true;
+            this.r4.Text = "Abril";
+            this.r4.UseVisualStyleBackColor = true;
+            this.r4.CheckedChanged += new System.EventHandler(this.r4_CheckedChanged);
+            // 
+            // r5
+            // 
+            this.r5.AutoSize = true;
+            this.r5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r5.Location = new System.Drawing.Point(6, 152);
+            this.r5.Name = "r5";
+            this.r5.Size = new System.Drawing.Size(70, 24);
+            this.r5.TabIndex = 4;
+            this.r5.TabStop = true;
+            this.r5.Text = "Mayo";
+            this.r5.UseVisualStyleBackColor = true;
+            this.r5.CheckedChanged += new System.EventHandler(this.r5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton6.Location = new System.Drawing.Point(6, 178);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(70, 24);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Junio";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // r7
+            // 
+            this.r7.AutoSize = true;
+            this.r7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r7.Location = new System.Drawing.Point(140, 48);
+            this.r7.Name = "r7";
+            this.r7.Size = new System.Drawing.Size(65, 24);
+            this.r7.TabIndex = 6;
+            this.r7.TabStop = true;
+            this.r7.Text = "Julio";
+            this.r7.UseVisualStyleBackColor = true;
+            this.r7.CheckedChanged += new System.EventHandler(this.r7_CheckedChanged);
+            // 
+            // r8
+            // 
+            this.r8.AutoSize = true;
+            this.r8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r8.Location = new System.Drawing.Point(140, 74);
+            this.r8.Name = "r8";
+            this.r8.Size = new System.Drawing.Size(82, 24);
+            this.r8.TabIndex = 7;
+            this.r8.TabStop = true;
+            this.r8.Text = "Agosto";
+            this.r8.UseVisualStyleBackColor = true;
+            this.r8.CheckedChanged += new System.EventHandler(this.r8_CheckedChanged);
+            // 
+            // r9
+            // 
+            this.r9.AutoSize = true;
+            this.r9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r9.Location = new System.Drawing.Point(140, 100);
+            this.r9.Name = "r9";
+            this.r9.Size = new System.Drawing.Size(115, 24);
+            this.r9.TabIndex = 8;
+            this.r9.TabStop = true;
+            this.r9.Text = "Septiembre";
+            this.r9.UseVisualStyleBackColor = true;
+            this.r9.CheckedChanged += new System.EventHandler(this.r9_CheckedChanged);
+            // 
+            // r10
+            // 
+            this.r10.AutoSize = true;
+            this.r10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r10.Location = new System.Drawing.Point(140, 126);
+            this.r10.Name = "r10";
+            this.r10.Size = new System.Drawing.Size(90, 24);
+            this.r10.TabIndex = 9;
+            this.r10.TabStop = true;
+            this.r10.Text = "Octubre";
+            this.r10.UseVisualStyleBackColor = true;
+            this.r10.CheckedChanged += new System.EventHandler(this.r10_CheckedChanged);
+            // 
+            // r11
+            // 
+            this.r11.AutoSize = true;
+            this.r11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r11.Location = new System.Drawing.Point(140, 152);
+            this.r11.Name = "r11";
+            this.r11.Size = new System.Drawing.Size(110, 24);
+            this.r11.TabIndex = 10;
+            this.r11.TabStop = true;
+            this.r11.Text = "Noviembre";
+            this.r11.UseVisualStyleBackColor = true;
+            this.r11.CheckedChanged += new System.EventHandler(this.r11_CheckedChanged);
+            // 
+            // r12
+            // 
+            this.r12.AutoSize = true;
+            this.r12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.r12.Location = new System.Drawing.Point(140, 178);
+            this.r12.Name = "r12";
+            this.r12.Size = new System.Drawing.Size(107, 24);
+            this.r12.TabIndex = 11;
+            this.r12.TabStop = true;
+            this.r12.Text = "Diciembre";
+            this.r12.UseVisualStyleBackColor = true;
+            this.r12.CheckedChanged += new System.EventHandler(this.r12_CheckedChanged);
+            // 
+            // Combo
+            // 
+            this.Combo.BackColor = System.Drawing.SystemColors.Menu;
+            this.Combo.FormattingEnabled = true;
+            this.Combo.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.Combo.Location = new System.Drawing.Point(526, 84);
+            this.Combo.Name = "Combo";
+            this.Combo.Size = new System.Drawing.Size(121, 24);
+            this.Combo.TabIndex = 1;
+            this.Combo.SelectedIndexChanged += new System.EventHandler(this.Combo_SelectedIndexChanged);
+            // 
+            // lbli
+            // 
+            this.lbli.AutoSize = true;
+            this.lbli.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbli.Location = new System.Drawing.Point(424, 49);
+            this.lbli.Name = "lbli";
+            this.lbli.Size = new System.Drawing.Size(364, 32);
+            this.lbli.TabIndex = 2;
+            this.lbli.Text = "Ingrese día de nacimiento";
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn1.Location = new System.Drawing.Point(510, 286);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(147, 89);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "Obtener Signo Zodiacal";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lbli);
+            this.Controls.Add(this.Combo);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton r1;
+        private System.Windows.Forms.RadioButton r12;
+        private System.Windows.Forms.RadioButton r11;
+        private System.Windows.Forms.RadioButton r10;
+        private System.Windows.Forms.RadioButton r9;
+        private System.Windows.Forms.RadioButton r8;
+        private System.Windows.Forms.RadioButton r7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton r5;
+        private System.Windows.Forms.RadioButton r4;
+        private System.Windows.Forms.RadioButton r3;
+        private System.Windows.Forms.RadioButton r2;
+        private System.Windows.Forms.ComboBox Combo;
+        private System.Windows.Forms.Label lbli;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ Examen/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Combo.Items.Add("1");
+            Combo.Items.Add("2");
+            Combo.Items.Add("3");
+            Combo.Items.Add("4");
+            Combo.Items.Add("5");
+            Combo.Items.Add("6");
+            Combo.Items.Add("7");
+            Combo.Items.Add("8");
+            Combo.Items.Add("9");
+            Combo.Items.Add("10");
+            Combo.Items.Add("11");
+            Combo.Items.Add("12");
+            Combo.Items.Add("13");
+            Combo.Items.Add("14");
+            Combo.Items.Add("15");
+            Combo.Items.Add("16");
+            Combo.Items.Add("17");
+            Combo.Items.Add("18");
+            Combo.Items.Add("19");
+            Combo.Items.Add("20");
+            Combo.Items.Add("21");
+            Combo.Items.Add("22");
+            Combo.Items.Add("23");
+            Combo.Items.Add("24");
+            Combo.Items.Add("25");
+            Combo.Items.Add("26");
+            Combo.Items.Add("27");
+            Combo.Items.Add("28");
+            Combo.Items.Add("29");
+            Combo.Items.Add("30");
+            Combo.Items.Add("31");
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void r2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r7_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r8_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r9_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r10_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r11_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void r12_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
